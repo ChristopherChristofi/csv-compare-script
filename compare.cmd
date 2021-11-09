f77a4f17de860a273a3e628cd05d70d90b64452a
christopher-christofi <christopherlchristofi@outlook.com>
2021-11-09 14:21:19 +0000

    Update initial parse comparison merge, update intermediary temp file properties, update file comparison buffer

--- compare.cmd
+++ compare.cmd
@@ -7,7 +7,7 @@ set "_merge=updated"
 set "comp_doc=complete"
 set "_uid1=idlist1"
 set "_uid2=idlist2"
-type nul > %_work2%.csv
+type nul > %_work2%.csv && attrib +h %_work2%.csv
 REM read of fixed file containing changes to be merged
 REM with new dataset file
 for /f %%a in ('dir /b *.csv') do (
@@ -16,38 +16,32 @@ for /f %%a in ('dir /b *.csv') do (
         call :reformat %%a , %_work2%.csv
     ))
 )
-type nul > %_new2%.csv
+type nul > %_merge%.csv && attrib +h %_merge%.csv
+type nul > %_new2%.csv && attrib +h %_new2%.csv
+type nul > %_uid1%.csv && attrib +h %_uid1%.csv
+type nul > %_uid2%.csv && attrib +h %_uid2%.csv
 call :reformat %new_doc%.csv , %_new2%.csv
-type nul > %_merge%.csv
-sort /uniq %_work2%.csv >nul
-sort /uniq %_new2%.csv >nul
-for /f "tokens=1-5 delims=," %%a in (%_work2%.csv) do (
-    for /f "delims=," %%f in (%_new2%.csv) do (
-        if %%a equ %%f ( echo %%a,%%b,%%c,%%d,%%e >> %_merge%.csv )
-    )
+for /f "tokens=1-5 delims=," %%a in (%_new2%.csv) do (
+    for /f "tokens=1-5 delims=," %%f in (%_work2%.csv) do (
+        if %%a equ %%f ( echo %%a,%%b,%%c,%%d,%%j >> %_merge%.csv ))
 ) || (
     echo Error in File Read and Comparing Datasets &Exit /b 1
 )
-type nul > %_uid1%.csv
-type nul > %_uid2%.csv
 REM subroutine calls to extract uinque id list comparatives
 call :extract %_new2%.csv %_uid1%.csv
 call :extract %_merge%.csv %_uid2%.csv
 REM comparison method to filter differences
-for /f %%a in ('fc %_uid1%.csv %_uid2%.csv ^| find /i "cod"') do (
+for /f %%a in ('type %_uid1%.csv ^| find /i "cod" /c') do set ln_cnt=%%a
+for /f %%a in ('fc /lb %ln_cnt% %_uid1%.csv %_uid2%.csv ^| find /i "cod"') do (
     for /f "tokens=1-5 delims=," %%b in (%_new2%.csv) do (
-        if %%a equ %%b (
-            echo %%b,%%c,%%d,%%e,%%f >> %_merge%.csv
-        )
-    )
+        if %%a equ %%b ( echo %%b,%%c,%%d,%%e,%%f >> %_merge%.csv ))
 ) || (
     echo Error in File Read and Comparing Datasets &Exit /b 1
 )
-
 type nul > %comp_doc%.csv
 echo uid,datetime,text,num, > %comp_doc%.csv
 type %_merge%.csv >> %comp_doc%.csv
-del %_new2%.csv %_work2%.csv %_merge%.csv %_uid1%.csv %_uid2%.csv
+del /A:H %_merge%.csv %_new2%.csv %_work2%.csv %_uid1%.csv %_uid2%.csv
 endlocal
 pause
 exit
