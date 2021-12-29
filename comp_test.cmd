b1a3903b9f66ecc8d9d02fd4a397ff9ccf23d2e4
christopher-christofi <christopherlchristofi@outlook.com>
2021-12-29 22:02:40 +0000

    Update testing validation script

--- comp_test.cmd
+++ comp_test.cmd
@@ -1,18 +1,20 @@
 @echo off
 Setlocal EnableDelayedExpansion
 set "new_doc=new"
-set "comp_doc=complete2"
+set "comp_doc=complete"
 set "list1=test_list1"
 set "list2=test_list2"
 if exist %new_doc%.csv (
     type nul > test_new.csv && attrib +h test_new.csv
+    type nul > test_comp.csv && attrib +h test_comp.csv
     call :reformat %new_doc%.csv , test_new.csv
-) else (( echo File Error: %new_doc%.csv not found. ) && del test_new.csv && pause &exit /b 1 )
-if exist %comp_doc%.csv ( if exist test_new.csv (
+    call :reformat %comp_doc%.csv , test_comp.csv
+) else (( echo File Error: %new_doc%.csv not found. ) && pause &exit /b 1 )
+if exist test_comp.csv ( if exist test_new.csv (
     type nul > %list1%.csv && attrib +h %list1%.csv
     type nul > %list2%.csv && attrib +h %list2%.csv
     call :extract test_new.csv , %list1%.csv , "tokens=1 delims=,"
-    call :extract %comp_doc%.csv , %list2%.csv , "skip=1 tokens=1 delims=,"
+    call :extract test_comp.csv , %list2%.csv , "tokens=1 delims=,"
     sort /uniq %list1%.csv /o test_list11.csv && attrib +h test_list11.csv
     sort /uniq %list2%.csv /o test_list22.csv && attrib +h test_list22.csv
 )) else (( echo File Error: %comp_doc%.csv not found. ) && del /A:H test_new.csv && pause &exit /b 1 )
@@ -20,7 +22,7 @@ if exist %comp_doc%.csv ( if exist test_new.csv (
 echo Missing codes found: > test_error.csv
 for /f %%a in ('fc test_list11.csv test_list22.csv ^| find /i "cod"') do ( echo %%a >> test_error.csv )
 
-del /A:H %list1%.csv %list2%.csv test_list11.csv test_list22.csv test_new.csv
+del /A:H %list1%.csv %list2%.csv test_list11.csv test_list22.csv test_new.csv test_comp.csv
 pause
 exit
 
